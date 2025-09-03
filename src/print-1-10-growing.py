9016290669b46037c4f3bf485a2e559e9ea75f42
realpepsiman <nuffe445@gmail.com>
2025-09-03 11:27:44 +0200

    Update print-1-10-growing.py

--- src/print-1-10-growing.py
+++ src/print-1-10-growing.py
@@ -2,9 +2,9 @@
 # Print the numbers described in the exercise
 x = ""
 for i in range(1, 11):
-    if i == 10:
+    if i == 1:
         x += str(i)
     else:
-        x += str(i) + " "
+        x +=  " " + str(i)
     print(x)
 
